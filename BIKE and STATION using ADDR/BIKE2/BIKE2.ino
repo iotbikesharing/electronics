46ef0d2348f059294be1d5355dd31a555dae5d87
minhden <darklantern28146@gmail.com>
2017-08-19 15:26:43 +0700

    Add files via upload

---
+++ BIKE and STATION using ADDR/BIKE2/BIKE2.ino
@@ -0,0 +1,75 @@
+#include <SoftwareSerial.h>
+#define Tx_Led 9
+#define Aux_Led 10
+#define Button 11
+#define AUX 6
+
+SoftwareSerial mySerial(2, 3); // (2-RX) <<-- Connect TX pin of LoRa  ......  (3-TX) <<-- Connect RX pin of LoRa
+// gnd SET_A and SET_B for Normal Mode (Send and Receive)
+int num = 333;
+
+char data1[100] = {0x00, 0x00, 0x17,2,10,7,2,9,5,4,6,10,6,69,60,32,99,99,99,94};                   // Create an array to store incoming data
+//char data1[100] = {0x000017,3};
+char data[100];
+String str (data1);
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  pinMode(Tx_Led, OUTPUT);
+  pinMode(Aux_Led, OUTPUT);
+  pinMode(Button,INPUT);
+  pinMode(AUX, INPUT);
+
+}
+
+void loop() 
+{
+  // Sending the dummy data
+  if (digitalRead(Button) == HIGH) 
+  {
+    digitalWrite(Tx_Led, HIGH);
+    mySerial.write (data1,100);
+    digitalWrite(Tx_Led, LOW);
+    //delay (6000);
+    //delay (3500);
+    delay (90000);  
+  }
+
+  //Receive the confirmation from the station and print it
+  if (mySerial.available() > 1)
+  {
+    String input = mySerial.readString();       // Read input from buffer like normal     
+   // Serial.println (input);
+    input.toCharArray(data,100);                // Convert input type Strin0g to store into array data[]
+
+    //delay (2000);
+    switch (data [2]) 
+      {
+        case 0:
+          Serial.println ("Transmission failed ... Re-sending message");
+          mySerial.write (data1, 100);
+          delay (1000);
+          break;
+
+        
+        case 2:
+          Serial.println ("Data sent");
+          data [2] = 0;
+          break;
+    }
+
+  }
+  //Serial.println (digitalRead (AUX));
+  // LED for checking if the module is receiving or sending data
+  if (digitalRead(AUX) == LOW) 
+  {
+    digitalWrite(Aux_Led, HIGH);
+  }
+  
+  else if (digitalRead(AUX) == HIGH) 
+  {
+    digitalWrite(Aux_Led, LOW);   
+  }
+  delay(1000);
+}     
---
+++ BIKE and STATION using ADDR/STATION/STATION.ino
@@ -0,0 +1,114 @@
+#include <SoftwareSerial.h>
+#define Tx_Led 9
+#define Rx_Led 10
+#define Button 11
+#define AUX 6
+
+SoftwareSerial mySerial(2, 3); // (2-RX) <<-- Connect TX pin of LoRa  ......  (3-TX) <<-- Connect RX pin of LoRa
+// gnd SET_A and SET_B for Normal Mode (Send and Receive)
+
+//String num = "3333";
+//char data [1000];
+
+char data1[100] = {0x00, 0x01, 0x17,1};
+char data2[100] = {0x00, 0x02, 0x17,2};
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  pinMode (Tx_Led, OUTPUT);
+  pinMode (Rx_Led, OUTPUT);
+  pinMode (Button, INPUT);
+  pinMode (AUX, INPUT);
+}
+
+void loop() {
+
+  if (digitalRead (Button) == HIGH) {
+    digitalWrite (Tx_Led, HIGH);
+   // mySerial.println(num);
+    mySerial.write (data1, 100);
+     //mySerial.write ("Received");
+    digitalWrite (Tx_Led, LOW);
+    delay (1500);
+  }
+
+  // Receive the data from the bikes and check which bike it is
+  // then send a confirmation signal
+  if (mySerial.available() > 1)
+  {
+
+        char data [1000];
+        String input = mySerial.readString();   
+        input.toCharArray (data,1000);
+        
+        switch (data [0]){
+
+          //BIKE 1
+          case 1:
+          
+          mySerial.write (data1, 100);
+          Serial.println ("Bike 1");
+          Serial.println ("Latitude: ");
+          Serial.print (data [1], DEC);
+          Serial.print (".");
+          for (int i =2; i< 9; i++) {
+              Serial.print (data[i],DEC);
+          }
+          Serial.println ("");
+          
+          Serial.println ("Longitude: ");
+          Serial.print (data [9],DEC);
+          Serial.print (data [10],DEC);
+          Serial.print (".");
+          for (int j= 11; j < 19; j++) {
+            Serial.print (data [j], DEC);
+          }
+          Serial.println (""); 
+          data [0] = 0;
+          delay (100);
+          break;
+
+          //BIKE 2
+          case 2:
+         
+          mySerial.write (data2, 100);
+          Serial.println ("Bike 2");
+          Serial.println ("Latitude: ");
+          Serial.print (data [1], DEC);
+          Serial.print (".");
+          for (int i =2; i< 7; i++) {
+             Serial.print (data[i],DEC);
+          }
+          Serial.println ("");
+          
+          Serial.println ("Longitude: ");
+          Serial.print (data [8],DEC);
+          Serial.print (data [9],DEC);
+          Serial.print (".");
+          for (int j= 10; j < 17; j++) {
+            Serial.print (data [j], DEC);
+          }
+          Serial.println (""); 
+          data [0] = 0;
+          delay (100);
+          break;
+
+        }
+  }
+
+  //Serial.println (digitalRead (AUX));
+
+  if (digitalRead (AUX) == LOW)
+  {
+    digitalWrite (Rx_Led, HIGH);
+  }
+
+  else if (digitalRead (AUX) == HIGH)
+  {
+    digitalWrite (Rx_Led, LOW);
+  }
+
+  delay(20);
+}
+
