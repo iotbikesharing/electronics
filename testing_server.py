ad154145757ca8f459ae7d4c4c08b7fdacc99b99
joebui <dienbuijoe@outlook.com>
2017-09-04 14:36:56 +0700

    modify code

--- testing_server.py
+++ testing_server.py
@@ -1,14 +1,19 @@
-
 import serial
 import paho.mqtt.client as mqtt
 
 ser = serial.Serial('/dev/ttyUSB0',9600)
-
 broker_address = '172.16.72.59'
+client = mqtt.Client("P1")
+client.connect(broker_address)
 
 while True:
-    data = ser.readline() 
+    data = ser.readline()
+    
+    if "UID:" in data:
+        client.publish('rfid', data)
+    elif "Bike 1" in data:
+        client.publish('lora_location', data)
+    else:
+        client.publish('lora_gps', data)
+
     print data
-    client = mqtt.Client("P1")
-    client.connect(broker_address)
-    client.publish('lora_gps', data)
