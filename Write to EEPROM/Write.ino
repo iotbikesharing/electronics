68bb086569a4ba05d90d543faa4d54b24c71562a
ThuanUong <kurotom257@gmail.com>
2017-09-04 14:32:31 +0700

    Add files via upload

---
+++ Write to EEPROM/Write.ino
@@ -0,0 +1,44 @@
+/*
+ * EEPROM Write
+ *
+ * Stores values read from analog input 0 into the EEPROM.
+ * These values will stay in the EEPROM when the board is
+ * turned off and may be retrieved later by another sketch.
+ */
+
+#include <EEPROM.h>
+
+/** the current address in the EEPROM (i.e. which byte we're going to write to next) **/
+int L[15] = {78,177,237,43,21,23,111,176,4,70,55,194,168,60,128};
+void setup() {
+ 
+}
+
+void loop() {
+  /***
+    Need to divide by 4 because analog inputs range from
+    0 to 1023 and each byte of the EEPROM can only hold a
+    value from 0 to 255.
+  ***/
+
+
+  /***
+    Write the value to the appropriate byte of the EEPROM.
+    these values will remain there when the board is
+    turned off.
+  ***/
+
+  unsigned int i = 0;
+      for (int addr=0; addr<15; addr++) {
+        EEPROM.update(addr, L[i]);        i++;
+      }
+  /***
+    As the EEPROM sizes are powers of two, wrapping (preventing overflow) of an
+    EEPROM address is also doable by a bitwise and of the length - 1.
+
+    ++addr &= EEPROM.length() - 1;
+  ***/
+
+
+  delay(100);
+}
